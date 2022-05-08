60d0298c353112d2f14488d239da36d14efe4507
theKianoush <keyonranjbar@aol.com>
2022-05-08 06:09:37 -0600

    Fire Project

---
+++ src/components/Sidebar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Sidebar.css'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import ChatIcon from '@mui/icons-material/Chat'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { Avatar, IconButton } from '@mui/material'
+import { SearchOutlined } from '@mui/icons-material'
+import SidebarChat from './SidebarChat'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+
+            <div className="sidebar__search">
+
+                <div className="sidebar__searchContainer">
+
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
+
+
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+export default Sidebar
